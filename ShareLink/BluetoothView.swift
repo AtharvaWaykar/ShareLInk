5a626bab22d5251a7d752f8888f6ca2c4bfc7047
AtharvaWaykar <atharvawaykar@gmail.com>
2024-08-18 23:49:42 -0400

    Added back Selection view

--- ShareLink/BluetoothView.swift
+++ ShareLink/BluetoothView.swift
@@ -1,1 +1,52 @@
+import SwiftUI
 
+
+struct BluetoothView: View {
+   
+    var body: some View {
+        
+        NavigationStack{
+            VStack {
+               
+                Text("Select a Device")
+                    .font(.title)
+                    .padding()
+                ScrollView {
+                    VStack {
+                      
+                        NavigationStack{
+                            
+                            ForEach(bluetoothManager.peripheralNames, id: \.self) { name in
+                                Text(name)
+                                    .padding()
+                                    .font(.body)
+                            }
+                        }
+                        .navigationTitle("Devices")
+                   
+                    }
+                    .padding()
+                   
+              }
+            
+                Spacer()
+            
+             
+                NavigationLink(destination: HomeView() ){
+                    Text("Home")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                
+                }
+                .padding(.horizontal)
+            }
+            .padding(.top)
+        }
+                        
+    }
+
+}
